e961689978234bc2cae488db0fb42ca0a6755bda
みやしたちひろ <c09069354869@gmail.com>
2019-07-07 13:58:56 +0800

    ロゴ画像の修正
    ロゴを変更したため

--- resources/views/shop/top2.blade.php
+++ resources/views/shop/top2.blade.php
@@ -4,17 +4,12 @@
 @section('content')
 <div class="container-fluid bg-img">
     <aside id="fh5co-hero">
-      <!-- <div class="flexslider js-fullwheight"> -->
-          
-            <!-- <div class="wrapper"> -->
               <div class="text-center">
                 <div class="slider-text-inner container1">
-                  <img alt="logo" src="storage/images/logo6.png" class="img2"><br>
+                  <img alt="logo" src="storage/images/logo2.png" class="img2"><br>
                 </div>
                   <a href="{{ url('home') }}" class="btn btn-primary btn-lg container2">はじめる</a>
               </div>
-            <!-- </div> -->
-        <!-- </div> -->
     </aside>
   </div>
 
