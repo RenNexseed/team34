d98cac8710748dae6a27a69f789dfe6244e1e726
鈴木蓮 <>
2019-07-23 15:15:37 +0800

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */public function boot()
     {
         if (\App::environment('heroku')) {
-        \URL::forceScheme('https');
+        (\URL::forceScheme('https');
     }
         Schema::defaultStringLength(191);
     }
