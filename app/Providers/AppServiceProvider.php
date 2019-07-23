b192acb15f149e8af0c05e24d678154c562c6869
鈴木蓮 <>
2019-07-23 14:53:41 +0800

    heroku編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */public function boot()
     {
+        if (\App::environment('heroku')) {
+        \URL::forceScheme('https');
+    }
         Schema::defaultStringLength(191);
     }
     /**
