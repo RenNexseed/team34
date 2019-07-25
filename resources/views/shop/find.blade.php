2efcd9d8e918d8ba2e95cdd35461f68195a558c5
和也山本 <kazuya2137@gmail.com>
2019-07-25 10:26:41 +0800

    表示の仕方を直した、find.blad.phpとopen.blade.phpのページレイアウトを整えた

--- resources/views/shop/find.blade.php
+++ resources/views/shop/find.blade.php
@@ -4,40 +4,30 @@
 
 <style>
   body {
-  font-family: Lato, 'Hiragino Kaku Gothic Pro', sans-serif;
-  font-weight: 400;
-  -webkit-font-smoothing: antialiased;
-  font-size: 30px;
-  letter-spacing: 0.05em;
-  color:black;
-  /*background-image: url(storage/images/backimg02.jpg);*/
+    font-weight: 700;
+    font-size: 30px;
+    letter-spacing: 0.05em;
+    background-color: #CCCCCC;
+  }
+
+  input{
+    height: 40px;
+  }
+  .row {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    /* margin-right: -15px; */
+    /* margin-left: -15px; */
+    width: auto;
 }
-
-input{ 
-  height: 40px;
-}
-
-h1{
-  font-size: 80px;
-  font-family: 'Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3','メイリオ',Meiryo,'ＭＳ Ｐゴシック',sans-serif;
-  color: #6cb4e4;
-  text-align: center;
-  padding: 0.25em;
-  border-top: solid 2px #6cb4e4;
-  border-bottom: solid 2px #6cb4e4;
-  background: -webkit-repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
-  background: repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
-}
-
-}
-
-
 </style>
 
 
-
+<body>
 @section('content')
-  <h1 class="text-center">商品検索画面</h1>
+  <h1 class="mb-5 mx-auto mt-5" style="color: black;">商品検索画面</h1>
   @include('layouts.side')
   <div class="container">
 
@@ -48,7 +38,7 @@ h1{
 
     </form>
       <!-- Button trigger modal -->
-<button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter">
+<button type="button" class="btn btn-hint" data-toggle="modal" data-target="#exampleModalCenter">
     ヒント
 </button>
 
@@ -74,7 +64,7 @@ h1{
   </div>
 </div>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter1">
+<button type="button" class="btn btn-hint" data-toggle="modal" data-target="#exampleModalCenter1">
   戻る
 </button>
 
@@ -103,16 +93,16 @@ h1{
       <div class="menu-items row text-center">
 
       <?php foreach ($products as $product): ?>
-        <div class="card col-4" style="border: double 5px #4ec4d3;">
-          <div class="img_wrap"><!-- ホバーを追加 -->
-          <img src="../{{ $product->picture_path }}" class="menu-item-image img-responsive" style="height:250px;">  <!-- ディレクトリ構造の違いによりimgのパスがhomeと違うので注意 -->
+        <div class="card col-3.8 m-1">
+          <div class="img_wrap mt-1"><!-- ホバーを追加 -->
+          <img src="../{{ $product->picture_path }}" class="menu-item-image img-responsive mt-1" style="height:250px;">  <!-- ディレクトリ構造の違いによりimgのパスがhomeと違うので注意 -->
         </div>
           <div class="card-body">
-            <h2 class="menu-item-name" style="background: linear-gradient(transparent 70%, #a7d6ff 70%);">{{ $product->name }}</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-                  <li class="list-group-item price" style="font-size: 24px">¥{{ $product->price}}</li>
-                  <li class="list-group-item">
+          <h2 class="menu-item-name moji2">{{ $product->name }}</h2>
+        </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item price moji2" style="font-size: 24px;background-color: white;">¥{{ $product->price}}</li>
+        <li class="list-group-item" style="background-color: white;">
                    <div class="modal_wrap">
         <input id="modal-{{ $product->id }}" type="checkbox">
             <div class="modal_overlay">
